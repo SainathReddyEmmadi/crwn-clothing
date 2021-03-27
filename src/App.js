fbbb8265711cd2f03a9248ad815406842d7f928f
sainath reddy <allussainathreddy@gmail.com>
2021-03-28 00:07:36 +0530

    divided homepage to components and styling done for homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import './App.css';
-import HomePage from './homepage.component'
-
+import React from "react";
+import HomePage from "./pages/homepage/homepage.component";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { sections } from "./directory.data";
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.styles.scss";
+export class Directory extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sections: sections,
+    };
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem title={title} imageUrl={imageUrl} key={id} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
