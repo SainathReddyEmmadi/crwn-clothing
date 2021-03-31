843a6eabf036e4323e62e1fa6af0f96d49442899
sainath reddy <allussainathreddy@gmail.com>
2021-03-31 14:45:53 +0530

    resolved cart-item count and added showing count in the basket on top header

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./cart-item.styles.scss";
 
-const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
+const CartItem = ({ item: { id, imageUrl, price, name, quantity } }) => {
   return (
-    <div className="cart-item">
+    <div className="cart-item" key={id}>
       <img src={imageUrl} alt={name} />
       <div className="item-details">
         <span className="name">{name}</span>
